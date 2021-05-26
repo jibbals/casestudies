0c5ea3330bb672e5974a916790e9a9283b183b5b
jibbals <jwg366@uowmail.edu.au>
2021-05-26 14:13:48 +1000

    added vorticity on multiple levels

--- python/vorticity.py
+++ python/vorticity.py
@@ -7,12 +7,11 @@ Created on Thu May  6 14:47:04 2021
 
 # plotting stuff
 import matplotlib.pyplot as plt
-#from matplotlib.ticker import FormatStrFormatter, LinearLocator#, LogFormatter
+from matplotlib import ticker
 #from matplotlib import colors
 import matplotlib
 #import matplotlib.patheffects as PathEffects
 import numpy as np
-from scipy import signal
 
 # local modules
 from utilities import plotting, utils, fio
@@ -22,13 +21,14 @@ from utilities import plotting, utils, fio
 ###
 
 
-def plot_vorticity(DA_u,DA_v,DA_ff=None,DA_topog=None):
+def plot_vorticity(DA_u,DA_v,DA_ff=None,DA_sh=None,DA_topog=None,
+                   **quiverwinds_args):
     """
     """
     # first find latitude and longitude
     if hasattr(DA_u,"longitude"):
-        lats=DA_u.latitude
-        lons=DA_u.longitude
+        lats=DA_u.latitude.values
+        lons=DA_u.longitude.values
     elif hasattr(DA_u,"lon"):
         lats=DA_u.lat.values
         lons=DA_u.lon.values
@@ -42,7 +42,13 @@ def plot_vorticity(DA_u,DA_v,DA_ff=None,DA_topog=None):
     else:
         u = DA_u.values
         v = DA_v.values
-
+    
+    # defaults for quivers
+    if "n_arrows" not in quiverwinds_args:
+        quiverwinds_args['n_arrows'] = 15
+    if "alpha" not in quiverwinds_args:
+        quiverwinds_args['alpha'] = .5
+    
     # Relative vorticity)
     rel_vor = utils.dragana_vorticity(lats,lons,u,v)
     
@@ -51,10 +57,13 @@ def plot_vorticity(DA_u,DA_v,DA_ff=None,DA_topog=None):
     avo_con = np.union1d(avo_con,-avo_con)
     avo_con = np.union1d(np.array([0]),avo_con)
     avo_map = plt.cm.RdBu_r
-    avo_norm = matplotlib.colors.SymLogNorm(2e-4,)#base=10)
+    if matplotlib.__version__ > '3.3.0':
+        avo_norm = matplotlib.colors.SymLogNorm(2e-4,base=10)
+    else:
+        avo_norm = matplotlib.colors.SymLogNorm(2e-4,)#base=10)
     
     # Modify colormap to have white for certain range
-    n=90
+    n = 90
     x = 0.5
     lower = avo_map(np.linspace(0, x, n))
     white = avo_map(np.ones(100)*0.5)
@@ -72,15 +81,22 @@ def plot_vorticity(DA_u,DA_v,DA_ff=None,DA_topog=None):
                       norm=avo_norm,
                       extend='both',
                       )
-    rv_bar=plt.colorbar(rv) # Add colorbar to the plot
+    
     if DA_ff is not None:
         plotting.map_fire(DA_ff.values,lats,lons)
     
+    if DA_sh is not None:
+        sh=DA_sh.values.T
+        plotting.map_sensibleheat(sh,
+                                  DA_sh.lat.values,
+                                  DA_sh.lon.values,
+                                  colorbar=False,
+                                  #zorder=3,
+                                  )
+    
+    # Add winds to plot
     plotting.quiverwinds(lats,lons,u,v,
-                         n_arrows=15,alpha=.5)
-    #qx = 5
-    #Q = plt.quiver(lon[::qx],lat[::qx],u10_wind.T[::qx,::qx],v10_wind.T[::qx,::qx],scale=1/0.02,units='inches')
-    #plt.quiverkey(Q, 0.1, 1.05, 5, r'$5 \frac{m}{s}$', labelpos='W', fontproperties={'weight': 'bold'})
+                         **quiverwinds_args)
 
     # topography
     if DA_topog is not None:
@@ -90,6 +106,8 @@ def plot_vorticity(DA_u,DA_v,DA_ff=None,DA_topog=None):
         else:
             topog = DA_topog.values
         plt.contour(lons,lats,topog,topog_con)
+    
+    return rv
 
 def vorticity_10m(
         mr,
@@ -129,42 +147,129 @@ def vorticity_10m(
         time_str=time_lt.strftime("%dT%H:%M")+"(UTC+%.2f)"%houroffset
                         
         ## FIRST FIGURE: 10m WIND DIR:
-        fig=plt.figure()
+        plt.figure()
         
-        plot_vorticity(DA_u10[ti], DA_v10[ti], DA_ff[ti], )#topog)
-        
+        rv=plot_vorticity(DA_u10[ti], DA_v10[ti], DA_ff[ti], )#topog)
+        # Add colorbar to the plot
+        plt.colorbar(rv) 
+        # add title, set aspect
         plt.title(mr + "\nvorticity (10m) " + time_str)
         plt.gca().set_aspect("equal")
             
         # save figure
         fio.save_fig(mr,"vorticity_10m", time_utc, plt, subdir=subdir)
         
-        # comparison figure:
-        #rel_vor2,_,_,_ = utils.vorticity(DA_u10[ti].values.T,
-        #                           DA_v10[ti].values.T,
-        #                           lats,lons)
-        #
-        #avo_con = 1e-4*2**np.arange(0,9)
-        #avo_con = np.union1d(avo_con,-avo_con)
-        #avo_con = np.union1d(np.array([0]),avo_con)
-        #avo_map = plt.cm.RdBu_r
-        #avo_norm = matplotlib.colors.SymLogNorm(2e-4,)#base=10)
-        #tmap=plotting.cmap_with_white_range(avo_map,n=30,x=0.5,)
-        #
-        #rv2=plt.contourf(lons,lats,rel_vor2, avo_con,
-        #                 cmap=tmap,
-        #                 norm=avo_norm,)
-        #
-        #plotting.map_fire(DA_ff[ti].values,lats,lons)
-        #plotting.quiverwinds(lats,lons,DA_u10[ti].values.T,DA_v10[ti].values.T,
-        #                     n_arrows=15,
-        #                     alpha=.5)
-        #
-        #plt.gca().set_aspect("equal")
-        #plt.colorbar(rv2)
-        #fio.save_fig(mr,"vorticity_10m_compare",time_utc,plt,subdir=subdir)
         
+
+def vorticity(mr, extent=None, subdir=None, levels=[3,10,20,30,40, 50,60,70,90], coastline=2):
+    """
+    ARGS:
+        mr: model run name
+        extent: [W,E,S,N] in degrees
+        subdir: name to save zoomed into
+        coastline: set to positive number to add coastline contour in metres
+    """
+    # Read firefront, heatflux (W/m2), U and V winds    
+    DS_fire = fio.read_model_run_fire(mr)
+    DA_topog = fio.model_run_topography(mr)
     
+    if extent is not None:
+        DS_fire = fio.extract_extent(DS_fire,extent)
+        DA_topog = fio.extract_extent(DA_topog,extent)
+        if subdir is None:
+            subdir=str(extent)
+    
+    lats=DS_fire.lat.data
+    lons=DS_fire.lon.data
+    if extent is None:
+        extent=[lons[0],lons[-1],lats[0],lats[-1]]
+        
+    #times=DS_fire.time.data
+    #localtimes = utils.local_time_from_time_lats_lons(times,lats,lons)
+    #houroffset=utils.local_time_offset_from_lats_lons(lats,lons)
+    
+    # loop over timesteps
+    hours=fio.hours_available(mr)
+    for hi,hour_utc in enumerate(hours):
+        
+        ## slice time
+        DS_atmos = fio.read_model_run_hour(mr,hour=hi)
+        times_utc = DS_atmos.time.values
+        
+        # read x and y winds
+        DA_x = DS_atmos['wnd_ucmp']
+        DA_y = DS_atmos['wnd_vcmp']
+        # destagger x and y winds
+        DA_u,DA_v = utils.destagger_winds_DA(DA_x,DA_y)
+        # subset to extent
+        if extent is not None:
+            DA_u = fio.extract_extent(DA_u, extent)
+            DA_v = fio.extract_extent(DA_v, extent)
+        
+        for ti, time_utc in enumerate(times_utc):
+            DS_fire_slice = DS_fire.sel(time=time_utc)
+            DA_ff  = DS_fire_slice['firefront']
+            DA_sh  = DS_fire_slice['SHEAT_2']
+            
+            ## get local time
+            time_lt = utils.local_time_from_time_lats_lons(time_utc,lats,lons)
+            houroffset = utils.local_time_offset_from_lats_lons(lats,lons)
+            time_str=time_lt.strftime("%dT%H:%M")+"(UTC+%.2f)"%houroffset
+                            
+            ## FIRST FIGURE: 10m WIND DIR:
+            fig = plt.figure(
+                figsize=[11,11],
+                )
+            #fig_grid = fig.add_gridspec(3, 3, wspace=0, hspace=0)
+            for li,level in enumerate(levels):
+                plt.subplot(3,3,1+li, aspect="equal")
+                if li == 0:
+                    in_sh = DA_sh
+                    in_ff = None
+                else:
+                    in_sh = None
+                    in_ff = DA_ff
+                cs_rv = plot_vorticity(DA_u[ti,li],DA_v[ti,li],
+                        DA_sh=in_sh,
+                        DA_ff=in_ff,
+                        add_quiver_key=(li==0),
+                        n_arrows=10,
+                        )
+                # add model level height average to each subplot
+                plt.text(0.01,0.01, # bottom left using axes coords
+                        "%.2f m"%DS_atmos.level_height[level].values,
+                        fontsize=9,
+                        transform=plt.gca().transAxes,# bottom left using axes coords
+                        zorder=10,
+                        )
+                plotting.map_add_locations_extent(extent,hide_text=True)
+                if coastline>0 and np.min(DA_topog.values)<coastline:
+                    plt.contour(lons,lats,DA_topog.values, np.array([coastline]),
+                                colors='k')
+                #plt.gca().set_aspect("equal")
+                plt.gca().set(xticks=[],yticks=[],aspect="equal")
+    
+            plt.suptitle(mr+" "+time_str)
+
+            plt.subplots_adjust(
+                    wspace = 0.0,  # the amount of width reserved for space between subplots,
+                    hspace = 0.0,
+                    )
+            #plt.tight_layout()
+            
+            # add space in specific area, then add vert wind colourbar
+            cbar_ax = fig.add_axes([0.05, 0.975, 0.25, 0.015]) # X Y Width Height
+            vort_ticks=[-1.28e-2, -0.16e-2, 0, 0.16e-2, 1.28e-2]
+            vort_ticks_str=["-1.28e-2", "-0.16e-2", "0", "0.16e-2", "1.28e-2"]
+            cbar = fig.colorbar(cs_rv, 
+                                cax=cbar_ax, 
+                                ticks=vort_ticks, 
+                                pad=0,
+                                orientation='horizontal',
+                                )
+            cbar.ax.set_xticklabels(vort_ticks_str,rotation=20)
+            # save figure
+            fio.save_fig(mr,"vorticity", time_utc, plt, subdir=subdir)
 
 if __name__ == '__main__':
 
@@ -175,9 +280,19 @@ if __name__ == '__main__':
     KI_zoom2_name = "zoom2"
     badja_zoom=[149.4,150.0, -36.4, -35.99]
     badja_zoom_name="zoom1"
+    
+    if True:
+        mr = "KI_run2_exploratory"
+        zoom=KI_zoom2
+        zoom_name=KI_zoom2_name
+        vorticity(mr,
+                  extent=zoom,
+                  subdir=zoom_name,
+                  levels=[1,2,3,5,8,10,12,14,18], # lower levels for exploratory
+                  )
 
-
-    mr="badja_run3"
-    zoom=badja_zoom
-    zoom_name=badja_zoom_name
-    vorticity_10m(mr,zoom,zoom_name)
+    if False:
+        mr="badja_run3"
+        zoom=badja_zoom
+        zoom_name=badja_zoom_name
+        vorticity_10m(mr,zoom,zoom_name)
